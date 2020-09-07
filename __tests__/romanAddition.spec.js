7a6fe2efb32ce8843dbd33182b5bc7ee7e6ba17e
ffaure32 <ffaure32@yahoo.fr>
2020-09-07 19:21:43 +0200

    on va jusqu a 4 largement

--- __tests__/romanAddition.spec.js
+++ __tests__/romanAddition.spec.js
@@ -7,30 +7,38 @@ describe("Roman Addition", () => {
         let result = calc.add("I", "I")
         // assert, or expect in this framework
         expect(result).toEqual("II");
-      });
-      test("add I and II", () => {
+    });
+    test("add I and II", () => {
         // arrange - the calc import does it
         // act
         let result = calc.add("I", "II")
         // assert, or expect in this framework
         expect(result).toEqual("III");
-      });
+    });
 
-      test("add II and I", () => {
+    test("add II and I", () => {
         // arrange - the calc import does it
         // act
         let result = calc.add("II", "I")
         // assert, or expect in this framework
         expect(result).toEqual("III");
-      });
+    });
 
-      test("add I and III", () => {
+    test("add I and III", () => {
         // arrange - the calc import does it
         // act
         let result = calc.add("I", "III")
-        // assert, or expect in this framework
+        // assert, or expect in this frame
         expect(result).toEqual("IV");
-      });
-
     });
+
+    test("add III and I", () => {
+        // arrange - the calc import does it
+        // act
+        let result = calc.add("III", "I")
+        // assert, or expect in this frame
+        expect(result).toEqual("IV");
+    });
+
+});
 
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const calc = {
     add: (firstNumber, secondNumber) => {
         if (firstNumber === "I" && secondNumber === "III") return "IV"
+        if (firstNumber === "III" && secondNumber === "I") return "IV"
         return firstNumber + secondNumber
     }
 }
